77f35431e3702bdd31a0ba1fbe42a1d3dc19bd35
André Luiz Lé Sénéchal Paolino <andre.senechal@hotmail.com>
2018-08-07 19:44:09 -0300

    feat(FriendList) Add Pagination.

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,13 +1,35 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
 import styles from './FriendList.css';
 import FriendListItem from './FriendListItem';
 
+
 class FriendList extends Component {
+  state = {
+    page: 0,
+  }
+  handlePageChange = (page) => {
+    this.setState((state => {
+      return page !== state.page ? {page} : {}
+    }))
+  }
   render () {
+    const { friends, pageSize } = this.props;
+    const { page } = this.state;
+    const pages = [...Array(Math.ceil(friends.length / pageSize )).keys()].map((value, index) => (
+      <li
+        onClick={() => {this.handlePageChange(value)}}
+        key={value}
+        className={[styles.pageNumber, index === page ? styles.currentPage : ''].join(' ')}>
+          {value+1}
+        </li>
+    ))
+
     return (
+      <Fragment>
       <ul className={styles.friendList}>
         {
-          this.props.friends.map((friend, index) => {
+          friends.slice(page, pageSize).map((friend, index) => {
             return (
               <FriendListItem
                 key={index}
@@ -19,6 +41,10 @@ class FriendList extends Component {
           })
         }
       </ul>
+      <ul className={styles.pageList}>
+        {pages}
+      </ul>
+      </Fragment>
     );
   }
 
@@ -26,7 +52,12 @@ class FriendList extends Component {
 
 FriendList.propTypes = {
   friends: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  pageSize: PropTypes.number,
 };
+
+FriendList.defaultProps = {
+  pageSize: 2,
+}
 
 export default FriendList;
